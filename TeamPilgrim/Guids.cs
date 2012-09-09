922048417e13ec8b67bc93b24305cd2eef7c0150
Stanley Goldman <Stanley.Goldman@gmail.com>
2012-09-09 17:06:13 -0400

    Initial TeamPilgrim project

---
+++ TeamPilgrim/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace JustAProgrammer.TeamPilgrim
+{
+    static class GuidList
+    {
+        public const string guidTeamPilgrimPkgString = "f56b8666-3ef2-46e5-ac26-8aa0efe1f484";
+        public const string guidTeamPilgrimCmdSetString = "064ac9aa-5aa9-498a-a5d9-2077be8d9ad6";
+        public const string guidToolWindowPersistanceString = "62e88c94-e7e0-4bdc-98ff-1e308fcda7fa";
+
+        public static readonly Guid guidTeamPilgrimCmdSet = new Guid(guidTeamPilgrimCmdSetString);
+    };
+}
---
+++ TeamPilgrim/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace JustAProgrammer.TeamPilgrim
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdTeamPilgrim =        0x100;
+        public const uint toolTeamPilgrim =    0x101;
+
+    };
+}
