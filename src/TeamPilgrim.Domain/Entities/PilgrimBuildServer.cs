5c288f14b04fa61aaec094264164b16ed316b249
Stanley Goldman <Stanley.Goldman@gmail.com>
2012-09-26 22:26:23 -0400

    Adding missing file

---
+++ src/TeamPilgrim.Domain/Entities/PilgrimBuildServer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.TeamFoundation.Build.Client;
+
+namespace JustAProgrammer.TeamPilgrim.Domain.Entities
+{
+    public class PilgrimBuildServer
+    {
+        public IBuildServer BuildServer { get; set; }
+    }
+}
