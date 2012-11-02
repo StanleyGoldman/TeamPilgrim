b7b472c091b3f2c68da6638903fe01fc1ab13a27
Stanley Goldman <Stanley.Goldman@gmail.com>
2012-11-02 19:17:46 -0400

    Improved QueryDefinitionModelContextMenu

--- src/TeamPilgrim.VisualStudio/Model/QueryItemModels/IQueryItemCommandModel.cs
+++ src/TeamPilgrim.VisualStudio/Model/QueryItemModels/IQueryItemCommandModel.cs
@@ -5,5 +5,7 @@ namespace JustAProgrammer.TeamPilgrim.VisualStudio.Model.QueryItemModels
     public interface IQueryItemCommandModel
     {
         RelayCommand<QueryDefinitionModel> OpenQueryDefinitionCommand { get; }
+        RelayCommand<QueryDefinitionModel> EditQueryDefinitionCommand { get; }
+        RelayCommand<QueryDefinitionModel> DeleteQueryDefinitionCommand { get; }
     }
 }
